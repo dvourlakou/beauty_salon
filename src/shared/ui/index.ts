39ba0d184759771582f22e03e6d9a6a58dc216cd
dvourlakou <dvourlakou0@gmail.com>
2026-08-07 09:32:09 +0300

    Διορθώσεις στο αρχειο shared/ui/index.ts στο κομμάτι των paths για Button και IconButton

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,6 @@
-export { default as Button } from './Button';
+export { default as Button } from './Βutton.tsx';
 
-export { default as IconButton } from './IconButton';
+export { default as IconButton } from './ΙconButton.tsx';
 
 export { default as Card } from './Card';
 
