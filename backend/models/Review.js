7cf02a9c718bfbea75d5f9491ab5f16740a7717a
dvourlakou <dvourlakou0@gmail.com>
2026-08-13 16:08:38 +0300

    Δημιουργία model Review.js

---
+++ backend/models/Review.js
@@ -0,0 +1,24 @@
+const {DataTypes} = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Review', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+                max: 5,
+            },
+        },
+        comment: {
+            type: DataTypes.TEXT,
+        },
+    }, {
+        timestamps: true,
+    });
+};
