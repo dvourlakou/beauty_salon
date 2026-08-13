979260b9c12e82247799332e422c4e6b0bf25fad
dvourlakou <dvourlakou0@gmail.com>
2026-08-13 13:11:24 +0300

    Κώδικας για το User.js

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./index');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    roles: {
+        type: DataTypes.ENUM('CUSTOMER', 'ADMIN', 'EMPLOYEE'),
+        defaultValue: 'CUSTOMER',
+    },
+}, {
+    timestamps: true,
+});
