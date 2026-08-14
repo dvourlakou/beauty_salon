23b4ca7ec7b9d30584a5ea3e688d1bb21172a3cd
dvourlakou <dvourlakou0@gmail.com>
2026-08-14 16:22:40 +0300

    Προσθήκη const serviceRoutes και app.use('/api/services',serviceRoutes)

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const { sequelize } = require('./models');
 
 //import routes
 const authRoutes = require('./routes/authRoutes');
+const serviceRoutes = require('./routes/serviceRoute');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +19,7 @@ app.use(express.json());
 
 //use routes
 app.use('/api/auth', authRoutes);
+app.use('/api/services',serviceRoutes);
 
 //test route
 app.get('/', (req,res) => {
