49ecef70618d081e111817b9ccc55662963de301
dvourlakou <dvourlakou0@gmail.com>
2026-08-14 15:11:20 +0300

    Σύνδεσα τα routes στο server.js

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,18 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { sequelize } = require('./models');
 
+//import routes
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+//use routes
+app.use('/api/auth', authRoutes);
 
 //test route
 app.get('/', (req,res) => {
