b6136108178dff127db151102a0ab5757c3bda4a
dvourlakou <dvourlakou0@gmail.com>
2026-08-16 20:32:25 +0300

    Έφτιαξα τα Routes για τις συναρτησεις του αρχείου appointmentContoller.js

---
+++ backend/routes/appointmentRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {createAppointment, getMyAppointment, cancelAppointment} = require('../controllers/appointmentContoller');
+const authMiddleware = require('../middleware/auth');
+
+const router = express.Router();
+
+
+//Όλα τα routes χρειάζονται authentication
+router.use(authMiddleware);
+
+router.post('/', createAppointment);
+router.get('/my', getMyAppointment);
+router.delete('/.id', cancelAppointment);
+
+module.exports = router;
