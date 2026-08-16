f972e078c88c6fde32e1e6e07456c3673f1ed895
dvourlakou <dvourlakou0@gmail.com>
2026-08-16 20:41:34 +0300

    Συνδέω τo appointmentRoute

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const { sequelize } = require('./models');
 //import routes
 const authRoutes = require('./routes/authRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
+const appointmentRoutes = require('./routes/appointmentRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,6 +21,7 @@ app.use(express.json());
 //use routes
 app.use('/api/auth', authRoutes);
 app.use('/api/services',serviceRoutes);
+app.use('/api/appointments', appointmentRoutes);
 
 //test route
 app.get('/', (req,res) => {
