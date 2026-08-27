be29bb9eb22fc19a987db4f953d51b7045dced8f
dvourlakou <dvourlakou0@gmail.com>
2026-08-27 19:42:24 +0300

    Διόρθωση σχολίων swagger για τα endpoints register και login καθώς υπήρχαν λάθη στη στοίχιση και η εντολή npm run dev στο backend δεν έτρεχε σωστα

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,8 +6,33 @@ const router = express.Router();
 /**
  * @swagger
  * /api/auth/register:
-
+ *   post:
+ *     summary: Εγγραφή νέου χρήστη
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - name
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Ο χρήστης εγγράφηκε με επιτυχία
+ *       400:
+ *         description: Το email χρησιμοποιείται ήδη
  */
+
+
 
 router.post('/register', register);
 
@@ -17,37 +42,34 @@ router.post('/register', register);
  * /api/auth/login:
  *   post:
  *     summary: Σύνδεση χρήστη
- *     tags: [Auth]
- *        requestBody:
- *          required: true
- *          content:
- *            application/json:
- *              schema:
- *                type: object
- *                required:
- *                  -email:
- *                  -password:
- *                properties:
- *                  email:
- *                    type: string
- *                    example: user@example.com
- *                  password:
- *                    type: string
- *                    example: "123456"
- *        responses:
- *          200:
- *            description: Επιτυχής σύνδεση
- *              content:
- *                application/json:
- *                  schema:
- *                    type: object
- *                    properties:
- *                      token:
- *                        type: string
- *                      user:
- *                        type: object
- *           401:
- *             description: Λάθος email ή κωδικός
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email:
+ *               - password:
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Επιτυχής σύνδεση
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *       401:
+ *         description: Λάθος email ή κωδικός
  */
 
 router.post('/login', login);
