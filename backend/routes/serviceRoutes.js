94c1c5d302b084acf305435b8bbd4427a4d9a19e
dvourlakou <dvourlakou0@gmail.com>
2026-08-27 20:13:33 +0300

    Δημιουργία σχολίων με swagger για τα endpoints /api/services/categories και /api/services/(id)

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -3,8 +3,38 @@ const {getAllCategories,getServiceById} = require('../controllers/serviceControl
 
 const router = express.Router();
 
-//public endpoints(δεν απαιτείται authentication)
+/**
+ * @swagger
+ * /api/services/categories:
+ *   get:
+ *     summary: Λήψη όλων των κατηγοριών με τις υπηρεσίες τους
+ *     responses:
+ *       200:
+ *         description: Λίστα κατηγοριών
+ */
+
+
 router.get('/categories', getAllCategories);
+
+/**
+ * @swagger
+ * /api/services/(id):
+ *   get:
+ *     summary: Λήψη μιας υπηρεσίας με το ID της
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Λεπτομέρειες της υπηρεσίας
+ *       400:
+ *         description: Η υπηρεσία δε βρέθηκε
+ *
+ */
+
 router.get('/id', getServiceById);
 
 module.exports = router;
