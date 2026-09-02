2350f33f1f9f49271577abb137f142f2a4384edd
dvourlakou <dvourlakou0@gmail.com>
2026-09-02 18:50:23 +0300

    Δημιουργία του employeeController.js

---
+++ backend/controllers/employeeController.js
@@ -0,0 +1,41 @@
+const {Employee, Service} = require('../models');
+
+//Λήψη των ενεργών υπαλλήλων
+const getAllEmployees = async (req,res) => {
+    try {
+        const employees = await Employee.findAll({
+            where: {isActive: true}
+        });
+        res.status(200).json(employees);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Παρουσιάστηκε σφάλμα κατά τη λήψη των υπαλλήλων'});
+    }
+};
+
+//Λήψη υπαλλήλων με βάσει το serviceId
+const getAllEmployeesByService = async (req,res) => {
+    try {
+        const {serviceId} = req.params;
+
+        //Αν Employee.belongsToMany(Service) τοτε
+        const service = await Service.findByPk(serviceId, {
+            include: [{ model: Employee, where: { isActive: true}, required: false}]
+        });
+
+        if (!service) {
+            return res.status(404).json({message: 'Η υπηρεσία δε βρέθηκε'});
+        }
+
+        //Αν δεν έχει δημιουργηθεί ακόμα η σχέση επιστρέφω όλους τους ενεργούς
+        const employees = service.Employee || await Employee.findAll({where: {isActive: true}});
+        res.status(200).json(employees);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Παρουσιάστηκε σφάλμα κατά τη λήψη των υπαλλήλων για την υπηρεσία'});
+    }
+};
+
+module.exports = {getAllEmployees,getAllEmployeesByService};
