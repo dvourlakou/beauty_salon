0a42fc7bcdd7adc4fc29dce0e7f6de7466fa24c5
dvourlakou <dvourlakou0@gmail.com>
2026-09-05 19:18:08 +0300

    Αλλαγή στη βασή δεδομένων λόγω των aliases και τροποποίηση συναρτήσεν

--- backend/seed.js
+++ backend/seed.js
@@ -5,13 +5,13 @@ const bcrypt = require('bcryptjs');
 const seedDatabase = async () => {
     try {
         // Καθαρίζω τη βάση
-        await sequelize.sync({ force: true });
+        await sequelize.sync({force: true});
 
-        // 1. Ίδιος κωδικός για test
+        //  Ίδιος κωδικός για test
         const hashedPassword = await bcrypt.hash('123456', 10);
 
-        // 2.Φτιάχνω τον  ADMIN
-        const adminUser = await User.create({
+        //Φτιάχνω τον  ADMIN
+        await User.create({
             name: 'Admin Salon',
             email: 'admin@salon.com',
             phone: '2100000000',
@@ -19,7 +19,7 @@ const seedDatabase = async () => {
             role: 'ADMIN'
         });
 
-        // 3. Φτιάχνω αρχικούς Πελάτες
+        //  Φτιάχνω αρχικούς Πελάτες
         const User1 = await User.create({
             name: 'Γιώργος Σακκάς',
             email: 'george@example.com',
@@ -36,85 +36,174 @@ const seedDatabase = async () => {
             role: 'USER'
         });
 
-        // 4. Φτιάχνω Κατηγορίες
-        const categories = await ServiceCategory.bulkCreate([
-            { name: 'Περιποίηση Νυχιών', description: 'Μανικιούρ και Πεντικιούρ' },
-            { name: 'Αποτρίχωση', description: 'Αποτρίχωση με κερί' },
-            { name: 'Μασάζ', description: 'Μασάζ χαλάρωσης' },
-        ]);
 
-        // 5.Υπηρεσιες ανά κατηγορία
-        const nailServices = await Service.bulkCreate([
-            { name: 'Μανικιούρ - Απλό', price: 18, categoryId: categories[0].id, durationMinutes: 30 },
-            { name: 'Μανικιούρ - Γαλλικό', price: 20, categoryId: categories[0].id, durationMinutes: 30 },
-            { name: 'Μανικιούρ - Ημιμόνιμο', price: 25, categoryId: categories[0].id, durationMinutes: 45 },
-            { name: 'Πεντικιούρ - Απλό', price: 22, categoryId: categories[0].id, durationMinutes: 40 },
-            { name: 'Πεντικιούρ - Γαλλικό', price: 25, categoryId: categories[0].id, durationMinutes: 45 },
-            { name: 'Πεντικιούρ - Ημιμόνιμο', price: 30, categoryId: categories[0].id, durationMinutes: 50 },
-        ]);
+        const User3 = await User.create({
+            name: 'Δήμητρα Βουρλάκου',
+            email: 'dimitra@example.com',
+            phone: '6944444444',
+            password: hashedPassword,
+            role: 'USER'
+        });
 
-        const waxingServices = await Service.bulkCreate([
-            { name: 'Αποτρίχωση - Μπικίνι', price: 40, categoryId: categories[1].id, durationMinutes: 20 },
-            { name: 'Αποτρίχωση - Χέρια', price: 20, categoryId: categories[1].id, durationMinutes: 15 },
-        ]);
-
-        const massageServices = await Service.bulkCreate([
-            { name: 'Μασάζ - Full Body 60\'', price: 28, categoryId: categories[2].id, durationMinutes: 60 },
-            { name: 'Μασάζ - Πλάτη 30\'', price: 15, categoryId: categories[2].id, durationMinutes: 30 },
-        ]);
+        //  Φτιάχνω Κατηγορίες
+        const categories = {
+            nails: await ServiceCategory.create({
+                name: 'Περιποίηση Νυχιών',
+                description: 'Μανικιούρ και Πεντικιούρ'
+            }),
+            waxing: await ServiceCategory.create({
+                name: 'Αποτρίχωση',
+                description: 'Αποτρίχωση με κερί'
+            }),
+            massage: await ServiceCategory.create({
+                name: 'Μασάζ',
+                description: 'Μασάζ χαλάρωσης'
+            })
+        };
 
 
-        // 6. Φτιάνω τα login των ασθητικών
-        const employeesData = [
-            { name: 'Αισθητικός 1', email: 'emp1@salon.com', phone: '6933333331', specialization: 'NAIL' },
-            { name: 'Αισθητικός 2', email: 'emp2@salon.com', phone: '6933333332', specialization: 'NAIL' },
-            { name: 'Αισθητικός 3', email: 'emp3@salon.com', phone: '6933333333', specialization: 'WAXING' },
-            { name: 'Αισθητικός 4', email: 'emp4@salon.com', phone: '6933333334', specialization: 'WAXING' },
-            { name: 'Αισθητικός 5', email: 'emp5@salon.com', phone: '6933333335', specialization: 'MASSAGE' },
-            { name: 'Αισθητικός 6', email: 'emp6@salon.com', phone: '6933333336', specialization: 'MASSAGE' },
-        ];
+        // Υπηρεσιες ανά κατηγορία
+        const services = {
+            simpleManicure: await Service.create({
+                name: 'Μανικιούρ - Απλό',
+                price: 18,
+                categoryId: categories.nails.id,
+                durationMinutes: 30
+            }),
+            frenchManicure: await Service.create({
+                name: 'Μανικιούρ - Γαλλικό',
+                price: 20,
+                categoryId: categories.nails.id,
+                durationMinutes: 30
+            }),
+            semiManicure: await Service.create({
+                name: 'Μανικιούρ - Ημιμόνιμο',
+                price: 25,
+                categoryId: categories.nails.id,
+                durationMinutes: 45
+            }),
+            simplePedicure: await Service.create({
+                name: 'Πεντικιούρ - Απλό',
+                price: 22,
+                categoryId: categories.nails.id,
+                durationMinutes: 40
+            }),
+            frenchPedicure: await Service.create({
+                name: 'Πεντικιούρ - Γαλλικό',
+                price: 25,
+                categoryId: categories.nails.id,
+                durationMinutes: 45
+            }),
+            semiPedicure: await Service.create({
+                name: 'Πεντικιούρ - Ημιμόνιμο',
+                price: 30,
+                categoryId: categories.nails.id,
+                durationMinutes: 50
+            }),
 
-        const createdEmployees = [];
+            bikiniWax: await Service.create({
+                name: 'Αποτρίχωση - Μπικίνι',
+                price: 40,
+                categoryId: categories.waxing.id,
+                durationMinutes: 20
+            }),
+            armsWax: await Service.create({
+                name: 'Αποτρίχωση - Χέρια',
+                price: 20,
+                categoryId: categories.waxing.id,
+                durationMinutes: 15
+            }),
 
-        for (const emp of employeesData) {
+
+            fullBodyMassage: await Service.create({
+                name: 'Μασάζ - Full Body 60\'',
+                price: 28,
+                categoryId: categories.massage.id,
+                durationMinutes: 60
+            }),
+            backMassage: await Service.create({
+                name: 'Μασάζ - Πλάτη 30\'',
+                price: 15,
+                categoryId: categories.massage.id,
+                durationMinutes: 30
+            })
+        };
+
+        const createEmployeeWithUser = async (name, email, phone, specialization) => {
             const user = await User.create({
-                name: emp.name,
-                email: emp.email,
-                phone: emp.phone,
+                name,
+                email,
+                phone,
                 password: hashedPassword,
                 role: 'EMPLOYEE'
             });
-
-            const newEmployee = await Employee.create({
-                name: emp.name,
-                specialization: emp.specialization,
+            return await Employee.create({
+                name,
+                specialization,
                 userId: user.id
             });
+        };
 
-            createdEmployees.push(newEmployee);
+
+        // Φτιάνω τα login των ασθητικών να κατηγορία
+        const employees = {
+            nail1: await createEmployeeWithUser(
+                'Αισθητικός 1',
+                'emp1@salon.com',
+                '6933333331',
+                'NAIL'
+            ),
+            nail2: await createEmployeeWithUser(
+                'Αισθητικός 2',
+                'emp2@salon.com',
+                '6933333332',
+                'NAIL'
+            ),
+            wax1: await createEmployeeWithUser(
+                'Αισθητικός 3',
+                'emp3@salon.com',
+                '6933333333',
+                'WAXING'
+            ),
+            wax2: await createEmployeeWithUser(
+                'Αισθητικός 4',
+                'emp4@salon.com',
+                '6933333333',
+                'WAXING'
+            ),
+            massage1: await createEmployeeWithUser(
+                'Αισθητικός 5',
+                'emp5@salon.com',
+                '6933333333',
+                'MASSAGE'
+            ),
+            massage2: await createEmployeeWithUser(
+                'Αισθητικός 6',
+                'emp6@salon.com',
+                '6933333333',
+                'MASSAGE'
+            ),
+        };
+
+        const nailStaff = [employees.nail1, employees.nail2];
+        const waxStaff = [employees.wax1, employees.wax2];
+        const massageStaff = [employees.massage1, employees.massage2];
+
+        for (const key of ['simpleManicure', 'frenchManicure', 'semiManicure', 'simplePedicure', 'frenchPedicure', 'semiPedicure']){
+            await services[key].addEmployees(nailStaff);
+        }
+
+        for(const key of ['bikiniWax', 'armsWax']){
+            await services[key].addEmployees(waxStaff);
+        }
+
+        for (const key of ['fullBodyMassage', 'BackMassage']){
+            await services[key].addEmployees(massageStaff);
         }
 
 
-        // 7.Αισθητικοί ανά ειδικότητα
-        const nailEmployees = createdEmployees.filter(e => e.specialization === 'NAIL');
-        const waxingEmployees = createdEmployees.filter(e => e.specialization === 'WAXING');
-        const massageEmployees = createdEmployees.filter(e => e.specialization === 'MASSAGE');
 
-        // 8. Αντιστοίχιση Υπηρεσιών με Αισθητικούς
-        for (const service of nailServices) {
-            await service.addEmployees(nailEmployees);
-        }
-
-        for (const service of waxingServices) {
-            await service.addEmployees(waxingEmployees);
-        }
-
-        for (const service of massageServices) {
-            await service.addEmployees(massageEmployees);
-        }
-
-        // 8. Ολοκληρωμένα test ραντεβού
-        // Περιλαμβάνει όλα τα απαραίτητα πεδία (userId, employeeId, serviceId, κτλ.)
+        // test ραντεβού
         const today = new Date().toISOString().split('T')[0];
 
         await Appointment.bulkCreate([
@@ -125,11 +214,11 @@ const seedDatabase = async () => {
                 customerName: User1.name,
                 customerEmail: User1.email,
                 phone: User1.phone,
-                price: nailServices[0].price,
-                durationMinutes: nailServices[0].durationMinutes,
+                price: services.simpleManicure.price,
+                durationMinutes: services.simpleManicure.durationMinutes,
                 userId: User1.id,
-                employeeId: createdEmployees[0].id,   
-                serviceId: nailServices[0].id
+                employeeId: employees.nail1.id,
+                serviceId: services.simpleManicure.id
             },
             {
                 date: today,
@@ -138,27 +227,28 @@ const seedDatabase = async () => {
                 customerName: User2.name,
                 customerEmail: User2.email,
                 phone: User2.phone,
-                price: massageServices[5].price,
-                durationMinutes: massageServices[5].durationMinutes,
+                price: services.fullBodyMassage.price,
+                durationMinutes: services.fullBodyMassage.durationMinutes,
                 userId: User2.id,
-                employeeId: createdEmployees[4].id,   
-                serviceId: massageServices[5].id
+                employeeId: employees.massage1.id,
+                serviceId: services.fullBodyMassage.id
             },
             {
                 date: today,
                 time: '15:00',
                 status: 'PENDING',
-                customerName: 'Δήμητρα Βουρλάκου',
-                customerEmail: 'dimitra@example.com',
-                phone: '6944444444',
-                price: waxingServices[3].price,
-                durationMinutes: waxingServices[3].durationMinutes,
-                userId: null,
-                serviceId: waxingServices[3].id
+                customerName: User3.name,
+                customerEmail: User3.email,
+                phone: User3.phone,
+                price: services.bikiniWax.price,
+                durationMinutes: services.bikiniWax.durationMinutes,
+                userId: User3.id,
+                employeeId: employees.wax1.id,
+                serviceId: services.bikiniWax.id
             }
         ]);
 
-        console.log('Database seeded successfully with users, services, employees, and initial appointments!');
+        console.log('Database seeded successfully with users, services, employees, and  appointments!');
         process.exit(0);
     } catch (error) {
         console.error('Error seeding database:', error);
